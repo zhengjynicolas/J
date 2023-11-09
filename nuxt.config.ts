176d33eef09a8426fd220dc867393e9a7c85ad8c
zhengjy0house <zhengjynicolas@hotmail.com>
2023-11-09 16:28:42 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+    devServer: {
+        port: 8080
+    },
     devtools: {
         enabled: process.env.NODE_ENV !== 'production'
     },
     modules: [
         '@nuxtjs/stylelint-module',
         '@nuxtjs/eslint-module',
+        '@nuxtjs/google-fonts',
+        '@nuxtjs/svg-sprite',
         '@nuxt/ui',
         'nuxt-lodash',
+        '@vueuse/nuxt',
         '@pinia/nuxt'
     ],
     stylelint:{
@@ -17,31 +23,38 @@ export default defineNuxtConfig({
     eslint: {
         lintOnStart: false
     },
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
+    // postcss: {
+    //     plugins: {
+    //         tailwindcss: {},
+    //         autoprefixer: {},
+    //     },
+    // },
     lodash: {
         prefix: "_",
         prefixSkip: ["string"],
         upperAfterPrefix: false,
-        exclude: ["map"],
-        // alias: [
-        //     ["camelCase", "stringToCamelCase"], // => stringToCamelCase
-        //     ["kebabCase", "stringToKebab"], // => stringToKebab
-        //     ["isDate", "isLodashDate"], // => _isLodashDate
-        // ],
+        exclude: ["map"]
     },
-    app: {
-        head: {
-            charset: 'utf-8',
-            viewport: 'width=device-width, initial-scale=1'
+    googleFonts:{
+        families: {
+            Roboto: true,
+            Inter: [400, 700],
+            'Josefin+Sans': true
         },
-        link: [],
-        style: [],
-        script: [],
-        noscript: []
+
+        display: 'swap' // 'auto' | 'block' | 'swap' | 'fallback' | 'optional'
     },
+    storybook: {
+        port: 6006,
+    },
+    // app: {
+    //     head: {
+    //         charset: 'utf-8',
+    //         viewport: 'width=device-width, initial-scale=1'
+    //     },
+    //     link: [],
+    //     style: [],
+    //     script: [],
+    //     noscript: []
+    // },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <NuxtLink to="/demo">Go To Demo</NuxtLink>
   <NuxtLink to="/doc">Go To Doc</NuxtLink>
+  <svg-icon name="love/bee-mine" />
+  <svg-icon name="nuxt" />
 </template>
 <script setup lang="ts">
 </script>
